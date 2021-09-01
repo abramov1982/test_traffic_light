4ae1a064d7017e356cd675fb43893048666a6a12
abramovio@it2.ru <abramovio@it2g.ru>
2021-09-01 15:59:01 +0300

    modify settings for prod

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ RUN pip install --upgrade pip
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
+RUN python manage.py collectstatic --noinput
--- test_traffic_light/settings.py
+++ test_traffic_light/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'traffic_light',
         'USER': 'traffic_light',
         'PASSWORD': '528555',
-        'HOST': 'traffic_light_bd'
+        'HOST': 'traffic_light_db'
     }
 }
 
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
